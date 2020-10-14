dbab03a9dd16d85e509213d0573ddd3b0df58bc8
yogitakumar <helloyogita@gmail.com>
2020-10-14 18:13:16 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/yogita/logindemo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.yogita.logindemo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/HomePage.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: Center(
+        child: Container(
+          height: 80,
+          width: 150,
+          decoration: BoxDecoration(
+              color: Colors.blue, borderRadius: BorderRadius.circular(10)),
+          child: FlatButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text(
+              'Welcome',
+              style: TextStyle(color: Colors.white, fontSize: 25),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
